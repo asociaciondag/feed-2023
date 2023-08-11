91d4ea0500bc9ce7f1a0a8348483b29163db59db
Oscar Otero <oom@oscarotero.com>
2023-08-11 18:31:45 +0200

    avances

--- _config.ts
+++ _config.ts
@@ -1,10 +1,14 @@
 import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
 import vento from "lume/plugins/vento.ts";
+import inline from "lume/plugins/inline.ts";
 
 const site = lume();
 
 site.use(postcss());
 site.use(vento());
+site.use(inline());
+site.copy("img");
+site.copy("isaac");
 
 export default site;
