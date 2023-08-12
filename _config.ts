d371430212c6f251763ae7228582caa925bf05f8
Oscar Otero <oom@oscarotero.com>
2023-08-12 11:24:48 +0200

    fixed path

--- _config.ts
+++ _config.ts
@@ -2,12 +2,14 @@ import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
 import vento from "lume/plugins/vento.ts";
 import inline from "lume/plugins/inline.ts";
+import basePath from "lume/plugins/base_path.ts";
 
 const site = lume();
 
 site.use(postcss());
 site.use(vento());
 site.use(inline());
+site.use(basePath());
 site.copy("img");
 site.copy("isaac");
 
