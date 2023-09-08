fd22d97bc1355e4307dee2f3387c5f287e012aa2
Oscar Otero <oom@oscarotero.com>
2023-09-08 13:20:04 +0200

    og

--- _config.ts
+++ _config.ts
@@ -3,6 +3,7 @@ import postcss from "lume/plugins/postcss.ts";
 import vento from "lume/plugins/vento.ts";
 import inline from "lume/plugins/inline.ts";
 import basePath from "lume/plugins/base_path.ts";
+import metas from "lume/plugins/metas.ts";
 
 const site = lume();
 
@@ -10,6 +11,7 @@ site.use(postcss());
 site.use(vento());
 site.use(inline());
 site.use(basePath());
+site.use(metas());
 site.copy("img");
 site.copy("isaac");
 
